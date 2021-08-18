ed30bb174d49e8cd35c9a06252a43e0bfba89026
giovana10 <88749160+giovana10@users.noreply.github.com>
2021-08-18 14:20:02 -0300

    Add files via upload

---
+++ giovana_210818.py
@@ -0,0 +1,27 @@
+hotday={"jazz":"ballet","eight":8,"thank":["giovana","rafael","sander","fernanda"]}
+print(hotday)
+#Question 4
+print("*****************************************************")
+print(hotday["jazz"])
+#Question 5 and 6
+print("*****************************************************")
+hotday["crocodile"]="zoo"
+coldday={"rafael":"bobo"}
+hotday["monkey"]=coldday
+print(hotday)
+#nested
+#Question 7 and 8
+print("*****************************************************")
+del hotday["jazz"]
+print(hotday)
+#Question 9 and 10
+print("*****************************************************")
+linda={}
+linda["crocodile"]="zoo"
+linda["chocolate"]="cacau"
+linda["limonada"]="limão"
+print(linda)
+
+
+
+
